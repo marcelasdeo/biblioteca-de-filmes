3fadd7079fb2da2477b932d3d60dc214d6057040
Marcela <marcela.sdeo4@gmail.com>
2023-11-28 08:30:32 -0300

    alterando o form
    adicionando os inputs e comentando o que é preciso fazer com cada um

--- src/components/Estilos.jsx
+++ src/components/Estilos.jsx
@@ -1,20 +1,20 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    logo: {
-        color: "black",
-        fontSize: 30,
+	logo: {
+		color: "black",
+		fontSize: 30,
 		fontWeight: "bold",
 		marginTop: 30,
 		marginBottom: 20,
-    },
-    titulo: {
-        color: "black",
-        fontSize: 20,
+	},
+	titulo: {
+		color: "black",
+		fontSize: 20,
 		fontWeight: "bold",
 		marginTop: 30,
 		marginBottom: 20,
-    },
+	},
 	container: {
 		backgroundColor: "red",
 		marginTop: 0,
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,26 +1,52 @@
-import { StyleSheet, Text, TouchableOpacity, View, TextInput} from "react-native";
+import { Text, TouchableOpacity, View, TextInput } from "react-native";
+import styles from "./Estilos";
 
 export default function Formulario() {
-    return (
-        <View>
-          <Text>Item para comprar</Text>
-          <View>
-            
-            <TextInput
-            
-              placeholder=""
-              
-            />
-            <TextInput
-              
-              placeholder=""
-              keyboardType=""
-              
-            />
-            <TouchableOpacity >
-              <Text>Salvar</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-    );
+	return (
+		<View>
+			
+			<View style={styles.container}>
+				<Text style={styles.logo}> MOVIES.LOG </Text>
+			</View>
+
+			<View>
+
+				{/* 'Assistido ou Para assistir':
+					input radio
+				*/}
+				<TextInput
+					placeholder="Data em que assistiu"
+					keyboardType=""
+				/>
+
+				<TextInput
+					placeholder="Nome do filme"
+					keyboardType=""
+				/>
+
+				{/* 'Data em que assistiu':
+					aparecer somente se o usuário selecionar 'Assistido' 
+					input para data
+				*/}
+				<TextInput
+					placeholder="Data em que assistiu"
+					keyboardType=""
+				/>
+
+				{/* 'Nota':
+					aparecer somente se o usuário selecionar 'Assistido' 
+					adicionar img de 5 estrelas
+					cada estrela terá um id, para identificar a nota passada pelo usuário
+				*/}
+				<TextInput
+					placeholder="Nota"
+					keyboardType=""
+				/>
+
+				<TouchableOpacity>
+					<Text> Adicionar </Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 }
